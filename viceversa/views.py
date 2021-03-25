bd9aa68ca2713954acdce4175c2c403716371605
s.nesterkov <s.nesterkov@rtk-service.ru>
2021-03-25 16:10:29 +0300

    Added word count

--- viceversa/views.py
+++ viceversa/views.py
@@ -6,4 +6,5 @@ def home(request):
 def reverse(request):
     user_text = request.GET['usertext']
     reversed_text = user_text[::-1]
-    return render(request, 'reverse.html', {'usertext': user_text, 'reversetext': reversed_text})
+    len_text = len(user_text.split())
+    return render(request, 'reverse.html', {'usertext': user_text, 'reversetext': reversed_text, 'lentext': len_text})
